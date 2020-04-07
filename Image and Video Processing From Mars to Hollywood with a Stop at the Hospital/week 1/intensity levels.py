aebb334587be863e17d3f52f65bee150808c8699
Chang-Chia-Chi <4759645@gmail.com>
2020-04-07 10:08:47 +0800

    Update intensity

--- Image and Video Processing From Mars to Hollywood with a Stop at the Hospital/week 1/intensity levels.py
+++ Image and Video Processing From Mars to Hollywood with a Stop at the Hospital/week 1/intensity levels.py
@@ -7,29 +7,39 @@ image = cv2.imread("prac.jpg")
 #cv2_imshow(image)
 
 def rgb2gray(image, bits = 8):
-  #將影像以numpy讀取灰階值
-  imgO = np.copy(image)
-  #cv2_imshow 讀取為uint8，可用astype做型態轉換，以利後續計算(如float64)
-  imgO = imgO.astype(np.float64,copy=False) #copy=False --> 不產生新陣列
-
-  #將R,G & B轉為256灰階(注意OpenCV讀入為BGR)，使用公式Gray = (R*299 + G*587 + B*114 + 500) / 1000
-  #若不想使用除法，可參考http://atlaboratary.blogspot.com/2013/08/rgb-g-rey-l-gray-r0.html，改用2進位
-  imgO[:,:,2] = imgO[:,:,2]*299  #(B)
-  imgO[:,:,1] = imgO[:,:,1]*587  #(G)
-  imgO[:,:,0] = imgO[:,:,0]*114  #(R)
-  imgG = (imgO.sum(axis = 2)+500)/1000
-  imgG = imgG.astype("uint8",copy=False)
-  #不同bits灰階計算
-  n = 8 - bits
-  if n == 0:
-    imgG
-  elif 0 < n <= 6:
-    imgG = ((imgG//(2**n))+0.5)*(2**n)
+  if type(bits) != int:
+    print("bits should be integer")
+    return 
+  elif bits > 8:
+    print("bits should be smaller than 8")
+    return 
+  elif bits <= 0:
+    print("bits should be positive int")
+    return 
   else:
-    filL = imgG <= 128
-    filU = imgG > 128
-    imgG[filL] = 0
-    imgG[filU] = 255
-  return imgG
+    #將影像以numpy讀取灰階值
+    imgO = np.copy(image)
+    #cv2_imshow 讀取為uint8，可用astype做型態轉換，以利後續計算(如float64)
+    imgO = imgO.astype(np.float64,copy=False) #copy=False --> 不產生新陣列
 
-cv2_imshow(rgb2gray(image,2))
+    #將R,G & B轉為256灰階(注意OpenCV讀入為BGR)，使用公式Gray = (R*299 + G*587 + B*114 + 500) / 1000
+    #若不想使用除法，可參考http://atlaboratary.blogspot.com/2013/08/rgb-g-rey-l-gray-r0.html，改用2進位
+    imgO[:,:,2] = imgO[:,:,2]*299  #(B)
+    imgO[:,:,1] = imgO[:,:,1]*587  #(G)
+    imgO[:,:,0] = imgO[:,:,0]*114  #(R)
+    imgG = (imgO.sum(axis = 2)+500)/1000
+    imgG = imgG.astype("uint8",copy=False)
+    #不同bits灰階計算
+    n = 8 - bits
+    if n == 0:
+      imgG
+    elif 0 < n <= 6:
+      imgG = ((imgG//(2**n))+0.5)*(2**n)
+    else:
+      filL = imgG <= 128
+      filU = imgG > 128
+      imgG[filL] = 0
+      imgG[filU] = 255
+    return imgG
+
+cv2_imshow(rgb2gray(image,8))
