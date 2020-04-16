ce90c1b0c8d50782b39362ef04a49f578b45eb2e
Chang-Chia-Chi <4759645@gmail.com>
2020-04-16 17:41:32 +0800

    add HS (haven't completed)

---
+++ Histogram Equalization & Specification/Histogram Specification.py
@@ -0,0 +1,6 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2 as cv2
+
+image = cv2.imread("C:/Users/USER/Desktop/Python/Image Processing/practice/Histogram Specification_source.jpg",0)
+
