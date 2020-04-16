cb2d16aab9af9c4cfe9162ccab075e43bf95246e
Chang-Chia-Chi <4759645@gmail.com>
2020-04-16 17:46:31 +0800

    Update Histogram Specification.py

--- Histogram Equalization & Specification/Histogram Specification.py
+++ Histogram Equalization & Specification/Histogram Specification.py
@@ -2,5 +2,4 @@ import numpy as np
 import matplotlib.pyplot as plt
 import cv2 as cv2
 
-image = cv2.imread("C:/Users/USER/Desktop/Python/Image Processing/practice/Histogram Specification_source.jpg",0)
-
+image = cv2.imread("C:/Users/USER/Desktop/Python/Image Processing/practice/Histogram Specification_source.jpg")
