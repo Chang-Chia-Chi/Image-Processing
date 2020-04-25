35cc7cb431b771d681a14b5257c3a1f281ef86e0
Chang-Chia-Chi <4759645@gmail.com>
2020-04-25 11:10:39 +0800

    modification for instruction

--- Fourier Transform in Image Processing/DFT/DFT.py
+++ Fourier Transform in Image Processing/DFT/DFT.py
@@ -2,7 +2,7 @@ import numpy as np
 
 def DFT(data):
     """
-    important concept learned here is if a number Nmn is a function of two variables
+    important concept learned here is if Nmn is a function of two variables m and n,
     create m (N X 1) and n (1 X N) array then apply function using these two array
     to obtain vectorized result.
     
